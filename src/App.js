a497fc57ffbe04c1df46573ee39ccc9772db5ece
Shahrukh <shahrukh.khan_ccv18@gla.ac.in>
2022-04-01 08:38:55 +0530

    Switch problem 56

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
-   <div className="App">
-       <h1>dfhdshhfdsh</h1>
-   </div>
-  )
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        {/* <Route exact path="/users">
+          <Users />
+        </Route> */}
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
-export default App
+export default App;
